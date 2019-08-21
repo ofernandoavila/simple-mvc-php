13279c07f00908d08b33c92867fc44bb1e51e894
Fernando Avila <fernandoavilajunior@gmail.com>
2019-08-21 17:39:00 -0300

    Simple MVC Framework made with PHP

---
+++ app/config/config.php
@@ -0,0 +1,16 @@
+<?php
+//DB Params
+define('DB_HOST', 'localhost');
+define('DB_USER', '__YOUT_USER__');
+define('DB_PASS', '__YOUR_PASS_');
+define('DB_NAME', '__YOUR_DBNAME__');
+
+//App Root
+
+define('APPROOT', dirname(dirname(__FILE__)));
+
+//URL Root
+define('URLROOT', '__YOUR_URL__');
+
+//Site Name
+define('SITENAME', '__YOUR_SITENAME__');
